a60691e29ed6e1a5d733c7261099cb5c74623ed4
Tomer Almog <talmog@spokeo.com>
2018-01-23 23:15:22 -0800

    fixed iframe promise issue

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
-import {Component, NgZone, ViewEncapsulation} from '@angular/core';
-import {CtaService, Promotion} from "../shared/cta.service";
-import {ActivatedRoute} from "@angular/router";
-import {Observable, Subscriber} from "rxjs";
+import { Component, NgZone, ViewEncapsulation } from '@angular/core';
+import { CtaService, Promotion } from "../shared/cta.service";
+import { ActivatedRoute } from "@angular/router";
+import { Observable, Subscriber } from "rxjs";
 
 declare var FB;
 interface FBWindow extends Window {
@@ -16,6 +16,8 @@ interface FBWindow extends Window {
   templateUrl: './home.component.html',
 
 })
+
+//src="//beta.msclvr.co/a/cta?preview=%7B%22slug%22%3A%22dHGJyg%22%2C%22artistName%22%3A%22Idan%20Raichel%22%2C%22collectionName%22%3A%22Idan%20Raichel%20-%20Piano%20-%20Songs%22%2C%22albumArtUrlSmall%22%3A%22http%3A%2F%2Fis1.mzstatic.com%2Fimage%2Fthumb%2FMusic128%2Fv4%2F0a%2F27%2F15%2F0a271558-ce7e-75f6-b5e7-c7ca2834dc1f%2Fsource%2F100x100bb.jpg%22%2C%22albumArtUrlMedium%22%3A%22http%3A%2F%2Fis1.mzstatic.com%2Fimage%2Fthumb%2FMusic128%2Fv4%2F0a%2F27%2F15%2F0a271558-ce7e-75f6-b5e7-c7ca2834dc1f%2Fsource%2F100x100bb.jpg%22%2C%22albumArtUrlLarge%22%3A%22http%3A%2F%2Fis1.mzstatic.com%2Fimage%2Fthumb%2FMusic128%2Fv4%2F0a%2F27%2F15%2F0a271558-ce7e-75f6-b5e7-c7ca2834dc1f%2Fsource%2F100x100bb.jpg%22%2C%22trackName%22%3A%22Ba'Yeshimon%22%2C%22iTunesLink%22%3A%22%22%2C%22callsToAction%22%3A%7B%22message%22%3A%22sdsd%22%2C%22type%22%3A%22facebook_follow%22%2C%22tag%22%3A%22%22%2C%22page%22%3A%22https%3A%2F%2Fwww.facebook.com%2Ftomer.almog.7%22%7D%7D
 
 export class HomeComponent {
   private static SOCIAL_CHANNEL_ACTION_TO_TYPE = {
@@ -45,23 +47,17 @@ export class HomeComponent {
     (<any>window).angularComponentRef = {component: this, zone: _ngZone};
     (<any>window).homeComponent = this;
 
-    // if (this._isPromotionInjected()) {
-    //   this._ctaService.previewMode = true;
-    //   this._promotionPromise = new Promise<void>(() => {
-    //     this.promotion = (<any>window).promotion;
-    //   });
-    //   return;
-    // }
-
     this._activatedRoute.queryParams.subscribe((queryParams: any) => {
       let preview = queryParams.preview;
       if (preview) {
         this._ctaService.previewMode = true;
-
-        this._promotionPromise = new Promise<void>(() => {
-          this.promotion = JSON.parse(preview);
-
-        });
+        const resolvePromise = (promise) => {
+          return promise.then(() => {
+            return this.promotion = JSON.parse(preview);
+          });
+        };
+        this._promotionPromise = Promise.resolve();
+        resolvePromise(this._promotionPromise);
 
       } else {
         this._slug = queryParams.slug;
@@ -78,7 +74,6 @@ export class HomeComponent {
   }
 
   ngOnInit() {
-    console.log(this._promotionPromise);
     if (this._ctaService.previewMode) {
       this.loading = false;
     }
@@ -95,7 +90,7 @@ export class HomeComponent {
           }
         }
       })
-      .catch(() => {
+      .catch((e) => {
         this.loading = false;
       });
   }
@@ -108,7 +103,7 @@ export class HomeComponent {
         this._initCTA();
       }
     })
-      .catch(() => {
+      .catch((e) => {
         this.loading = false;
       });
   }
@@ -187,6 +182,7 @@ export class HomeComponent {
   }
 
   private _initCTA() {
+    console.log('initCTA');
     this._intervalCount = 0;
     if (this.promotion) {
       if (this.promotion.callsToAction) {
